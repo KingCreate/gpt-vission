bf64f0e037418a21c82e9f60da453c8717e2f526
Nicholas Webster <nicholas.webster@staff.pentanet.com.au>
2024-08-21 15:05:26 +0800

    feat: use gpt-4o

--- src/lib/getImageChatCompletion.tsx
+++ src/lib/getImageChatCompletion.tsx
@@ -10,7 +10,7 @@ export async function getImageChatCompletion(
 
     const openai = new OpenAI({ apiKey, dangerouslyAllowBrowser: true })
     const completion = await openai.chat.completions.create({
-        model: 'gpt-4-turbo',
+        model: 'gpt-4o',
         messages: [
             {
                 role: 'user',
